1b9e9b2382635ffed8668c70859d5c113eb27604
Chris Foley <Foleychris@users.noreply.github.com>
2025-06-21 21:57:30 -0700

    Update irk_enrollment.h

--- custom_components/irk_enrollment/irk_enrollment.h
+++ custom_components/irk_enrollment/irk_enrollment.h
@@ -41,7 +41,7 @@ class IrkEnrollmentComponent :
   float get_setup_priority() const;
     void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
                            esp_ble_gatts_cb_param_t *param) override;
-   void	gap_event_handler (esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) override;
+   void	gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) override;
 
   void start();
   void stop();
